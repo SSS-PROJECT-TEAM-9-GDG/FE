b9575cad2f58d048aa86c2a74b75c7e2955b0a4d
choi-day <choisschoco@gmail.com>
2025-02-21 15:07:25 +0900

    브랜치 생성

---
+++ sssproject_frontend/lib/util/provider/backPressProvider.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BackPressProvider with ChangeNotifier {
+  DateTime? lastBackPressTime;
+  bool isDialog = false;
+
+  void onBackPress() {
+    DateTime now = DateTime.now();
+    if (lastBackPressTime == null || now.difference(lastBackPressTime!) > const Duration(seconds: 2)) {
+      lastBackPressTime = now;
+      isDialog = true;
+      notifyListeners();
+    } else {
+      SystemNavigator.pop();
+    }
+  }
+
+  void showDialogEnded() {
+    isDialog = false;
+    notifyListeners();
+  }
+}
