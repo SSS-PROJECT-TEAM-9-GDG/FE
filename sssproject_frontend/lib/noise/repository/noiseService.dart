510fbbe747add88d741eae9509a307dd09eac5de
choi-day <choisschoco@gmail.com>
2025-02-27 00:36:45 +0900

    fix: 이미지 서버 연결 코드 수정

--- sssproject_frontend/lib/noise/repository/noiseService.dart
+++ sssproject_frontend/lib/noise/repository/noiseService.dart
@@ -1,42 +1,4 @@
-// import 'package:dio/dio.dart';
-// import 'package:image_picker/image_picker.dart';
-// import 'package:sssproject_frontend/const/dio.dart';
-// import 'package:sssproject_frontend/noise/model/Noise.dart';
-
-// class NoiseService{
-//   final dio = Dio();
-
-//   Future<Noise> getNoiseData(String level, XFile image) async{
-//     Response response;
-//     try{
-//       String serverUrl = '$BASE_URL/api/noise/apply?level=$level';
-//       response = await dio.post(
-//         serverUrl,
-//         data: {'file' : image}
-//       );
-
-//       if(response.statusCode == 200) {
-//         return Noise.fromJson(response.data);
-//       } else {
-//         throw Exception('Fail to load data');
-//       }
-//     } on DioException catch (e) {
-//       String errorMessage = '서버 오류가 발생했습니다. 잠시 후 다시 시도해주세요.';
-//       if (e.response != null) {
-//         if (e.response!.statusCode == 500) {
-//           errorMessage = '서버에서 처리할 수 없는 오류가 발생했습니다.';
-//         }
-//         errorMessage = e.response?.data['message'] ?? errorMessage;
-//       } else {
-//         // 네트워크 오류나 기타 오류 처리
-//         errorMessage = e.message ?? errorMessage;
-//       }
-//       throw Exception('Failed to load report data: ${e.response?.data['message'] ?? e.message}');
-//     }
-//   }
-// }
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sssproject_frontend/const/dio.dart';
 
@@ -49,7 +11,7 @@ class NoiseService {
       return;
     }
 
-    final String url = '$BASE_URL/api/noise/apply?level=$level';
+    const String url = '$BASE_URL/api/noise/apply';
 
     try {
       FormData formData = FormData.fromMap({
@@ -60,6 +22,7 @@ class NoiseService {
 
       final Response response = await _dio.post(
         url,
+        queryParameters: {"level": level},
         data: formData,
       );
 
